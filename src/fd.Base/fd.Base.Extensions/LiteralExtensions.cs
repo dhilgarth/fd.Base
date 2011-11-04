2c12665b9343dfd8b97218d80e872b4310d3e10e
Daniel Hilgarth <d.hilgarth@fire-development.com>
2011-11-04 01:09:00 +0100

    Added literal time extensions for doubles

--- src/fd.Base/fd.Base.Extensions/LiteralExtensions.cs
+++ src/fd.Base/fd.Base.Extensions/LiteralExtensions.cs
@@ -28,5 +28,30 @@ namespace fd.Base.Extensions
         {
             return TimeSpan.FromDays(value);
         }
+
+        public static TimeSpan Seconds(this double value)
+        {
+            return TimeSpan.FromSeconds(value);
+        }
+
+        public static TimeSpan Milliseconds(this double value)
+        {
+            return TimeSpan.FromMilliseconds(value);
+        }
+
+        public static TimeSpan Minutes(this double value)
+        {
+            return TimeSpan.FromMinutes(value);
+        }
+
+        public static TimeSpan Hours(this double value)
+        {
+            return TimeSpan.FromHours(value);
+        }
+
+        public static TimeSpan Days(this double value)
+        {
+            return TimeSpan.FromDays(value);
+        }
     }
 }
