32a4df4078f01b2006f859352efed7ac745cc04d
Daniel Hilgarth <d.hilgarth@fire-development.com>
2011-11-11 21:06:46 +0100

    Commented Percentage class

--- src/fd.Base/fd.Base.Types/Percentage.cs
+++ src/fd.Base/fd.Base.Types/Percentage.cs
@@ -1,23 +1,66 @@
 ﻿namespace fd.Base.Types
 {
+    /// <summary>
+    /// Represents a percentage value in the narrow sense: 0% to 100%.
+    /// </summary>
     public class Percentage : Constrained<int>
     {
-        public Percentage() : base(Constraint) {}
-        public Percentage(int value) : base(Constraint, value) {}
-
-        private static bool Constraint(int v)
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Percentage"/> class.
+        /// </summary>
+        public Percentage()
+            : base(Constraint)
         {
-            return v >= 0 && v <= 100;
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Percentage"/> class.
+        /// </summary>
+        /// <param name="value">
+        /// The value.
+        /// </param>
+        public Percentage(int value)
+            : base(Constraint, value)
+        {
+        }
+
+        /// <summary>
+        /// Performs an implicit conversion from <see cref="System.Int32"/> to <see cref="fd.Base.Types.Percentage"/>.
+        /// </summary>
+        /// <param name="value">
+        /// The value.
+        /// </param>
+        /// <returns>
+        /// The result of the conversion.
+        /// </returns>
         public static implicit operator Percentage(int value)
         {
             return new Percentage(value);
         }
 
+        /// <summary>
+        /// Returns a <see cref="System.String"/> that represents this instance.
+        /// </summary>
+        /// <returns>
+        /// A <see cref="System.String"/> that represents this instance.
+        /// </returns>
         public override string ToString()
         {
             return Value + "%";
+        }
+
+        /// <summary>
+        /// Checks whether the specified value is a percentage in the narrow sense.
+        /// </summary>
+        /// <param name="v">
+        /// The value to be checked.
+        /// </param>
+        /// <returns>
+        /// <c>true</c> if the value represents one such narrow percentage; otherwise, <c>false</c>.
+        /// </returns>
+        private static bool Constraint(int v)
+        {
+            return v >= 0 && v <= 100;
         }
     }
 }
