5fb539e97795af5579beead6fda7c58f481d7af9
Daniel Hilgarth <d.hilgarth@fire-development.com>
2011-11-11 21:04:46 +0100

    Added UnitTestHelpers

---
+++ src/fd.Base/fd.Base.UnitTestHelpers/BaseContext.cs
@@ -0,0 +1,29 @@
+using Ploeh.AutoFixture;
+using Ploeh.AutoFixture.AutoNSubstitute;
+
+namespace fd.Base.UnitTestHelpers
+{
+    /// <summary>
+    /// The base class of all test contexts.
+    /// </summary>
+    public abstract class BaseContext
+    {
+        private readonly IFixture _fixture;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BaseContext"/> class.
+        /// </summary>
+        protected BaseContext()
+        {
+            _fixture = new Fixture().Customize(new AutoNSubstituteCustomization()).Customize(new StableFiniteSequenceCustomization());
+        }
+
+        /// <summary>
+        /// Gets the fixture.
+        /// </summary>
+        public IFixture Fixture
+        {
+            get { return _fixture; }
+        }
+    }
+}
---
+++ src/fd.Base/fd.Base.UnitTestHelpers/ISutFactory.cs
@@ -0,0 +1,19 @@
+﻿namespace fd.Base.UnitTestHelpers
+{
+    /// <summary>
+    /// Represents a factory that creates a system under test (SUT) instance.
+    /// </summary>
+    /// <typeparam name="TSut">
+    /// The type of the SUT.
+    /// </typeparam>
+    public interface ISutFactory<out TSut>
+    {
+        /// <summary>
+        /// Creates an SUT instance.
+        /// </summary>
+        /// <returns>
+        /// An instance of the SUT.
+        /// </returns>
+        TSut CreateSut();
+    }
+}
