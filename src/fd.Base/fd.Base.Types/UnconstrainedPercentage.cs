905fc23d16ea4d376137d71548b8d96474f453f5
Daniel Hilgarth <d.hilgarth@fire-development.com>
2011-11-11 21:07:04 +0100

    Added PositivePercentage and UnconstrainedPercentage types

---
+++ src/fd.Base/fd.Base.Types/UnconstrainedPercentage.cs
@@ -0,0 +1,52 @@
+namespace fd.Base.Types
+{
+    /// <summary>
+    /// Represents a percentage value in the wider sense.
+    /// </summary>
+    public class UnconstrainedPercentage : Constrained<double>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="UnconstrainedPercentage"/> class. 
+        /// </summary>
+        public UnconstrainedPercentage()
+            : base(x => true)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="UnconstrainedPercentage"/> class.
+        /// </summary>
+        /// <param name="value">
+        /// The value.
+        /// </param>
+        public UnconstrainedPercentage(double value)
+            : base(x => true, value)
+        {
+        }
+
+        /// <summary>
+        /// Performs an implicit conversion from <see cref="System.Int32"/> to <see cref="fd.Base.Types.UnconstrainedPercentage"/>.
+        /// </summary>
+        /// <param name="value">
+        /// The value.
+        /// </param>
+        /// <returns>
+        /// The result of the conversion.
+        /// </returns>
+        public static implicit operator UnconstrainedPercentage(double value)
+        {
+            return new UnconstrainedPercentage(value);
+        }
+
+        /// <summary>
+        /// Returns a <see cref="System.String"/> that represents this instance.
+        /// </summary>
+        /// <returns>
+        /// A <see cref="System.String"/> that represents this instance.
+        /// </returns>
+        public override string ToString()
+        {
+            return Value + "%";
+        }
+    }
+}
