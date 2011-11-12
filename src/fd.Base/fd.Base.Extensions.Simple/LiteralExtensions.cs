c5ce953626b47642e81960bcdc4a93df61041cdd
Daniel Hilgarth <d.hilgarth@fire-development.com>
2011-11-12 12:19:46 +0100

    Added comments to literal extensions

--- src/fd.Base/fd.Base.Extensions.Simple/LiteralExtensions.cs
+++ src/fd.Base/fd.Base.Extensions.Simple/LiteralExtensions.cs
@@ -4,51 +4,101 @@ namespace fd.Base.Extensions.Simple
 {
     public static class LiteralExtensions
     {
+        /// <summary>
+        /// Returns a TimeSpan representing the specified number of days.
+        /// </summary>
+        /// <param name="value">The value.</param>
+        /// <returns>The TimeSpan.</returns>
         public static TimeSpan Days(this int value)
         {
             return TimeSpan.FromDays(value);
         }
 
+        /// <summary>
+        /// Returns a TimeSpan representing the specified number of days.
+        /// </summary>
+        /// <param name="value">The value.</param>
+        /// <returns>The TimeSpan.</returns>
         public static TimeSpan Days(this double value)
         {
             return TimeSpan.FromDays(value);
         }
 
+        /// <summary>
+        /// Returns a TimeSpan representing the specified number of hours.
+        /// </summary>
+        /// <param name="value">The value.</param>
+        /// <returns>The TimeSpan.</returns>
         public static TimeSpan Hours(this int value)
         {
             return TimeSpan.FromHours(value);
         }
 
+        /// <summary>
+        /// Returns a TimeSpan representing the specified number of hours.
+        /// </summary>
+        /// <param name="value">The value.</param>
+        /// <returns>The TimeSpan.</returns>
         public static TimeSpan Hours(this double value)
         {
             return TimeSpan.FromHours(value);
         }
 
+        /// <summary>
+        /// Returns a TimeSpan representing the specified number of milliseconds.
+        /// </summary>
+        /// <param name="value">The value.</param>
+        /// <returns>The TimeSpan.</returns>
         public static TimeSpan Milliseconds(this int value)
         {
             return TimeSpan.FromMilliseconds(value);
         }
 
+        /// <summary>
+        /// Returns a TimeSpan representing the specified number of milliseconds.
+        /// </summary>
+        /// <param name="value">The value.</param>
+        /// <returns>The TimeSpan.</returns>
         public static TimeSpan Milliseconds(this double value)
         {
             return TimeSpan.FromMilliseconds(value);
         }
 
+        /// <summary>
+        /// Returns a TimeSpan representing the specified number of minutes.
+        /// </summary>
+        /// <param name="value">The value.</param>
+        /// <returns>The TimeSpan.</returns>
         public static TimeSpan Minutes(this int value)
         {
             return TimeSpan.FromMinutes(value);
         }
 
+        /// <summary>
+        /// Returns a TimeSpan representing the specified number of minutes.
+        /// </summary>
+        /// <param name="value">The value.</param>
+        /// <returns>The TimeSpan.</returns>
         public static TimeSpan Minutes(this double value)
         {
             return TimeSpan.FromMinutes(value);
         }
 
+        /// <summary>
+        /// Returns a TimeSpan representing the specified number of seconds.
+        /// </summary>
+        /// <param name="value">The value.</param>
+        /// <returns>The TimeSpan.</returns>
         public static TimeSpan Seconds(this int value)
         {
             return TimeSpan.FromSeconds(value);
         }
 
+        /// <summary>
+        /// Returns a TimeSpan representing the specified number of seconds.
+        /// </summary>
+        /// <param name="value">The value.</param>
+        /// <returns>The TimeSpan.</returns>
         public static TimeSpan Seconds(this double value)
         {
             return TimeSpan.FromSeconds(value);
