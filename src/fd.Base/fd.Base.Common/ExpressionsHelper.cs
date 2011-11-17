bfdaa76a6f7cdf27560f49131d9a33cb9591bf1a
Daniel Hilgarth <d.hilgarth@fire-development.com>
2011-11-17 21:04:12 +0100

    Added ExpressionsHelper to easily extract the name of a property or member from expressions of these forms: x => x.Name or () => Name

---
+++ src/fd.Base/fd.Base.Common/ExpressionsHelper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace fd.Base.Common
+{
+    public class ExpressionsHelper
+    {
+        public static PropertyInfo GetPropertyInfo<T>(Expression<T> propertyExpression)
+        {
+            var body = propertyExpression.Body as MemberExpression;
+            if (body == null)
+                throw new ArgumentException("'propertyExpression' should be a member expression, but it is a " + propertyExpression.Body.GetType());
+
+            var propertyInfo = body.Member as PropertyInfo;
+            if (propertyInfo == null)
+                throw new ArgumentException("The member used in the expression should be a property, but it is a " + body.Member.GetType());
+            return propertyInfo;
+        }
+
+        public static string GetPropertyName<TProperty>(Expression<Func<TProperty>> propertyExpression)
+        {
+            return GetPropertyInfo(propertyExpression).Name;
+        }
+
+        public static string GetPropertyName<TClass, TProperty>(Expression<Func<TClass, TProperty>> propertyExpression)
+        {
+            return GetPropertyInfo(propertyExpression).Name;
+        }
+    }
+}
