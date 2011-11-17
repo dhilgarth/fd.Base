608f05215134ec08f0281ff2565fdcf83b618bad
Daniel Hilgarth <d.hilgarth@fire-development.com>
2011-11-17 21:05:25 +0100

    Added ctor parameter to allow the creation of SQL files.

--- src/fd.Base/fd.Base.NHibernate/SchemaRecreator.cs
+++ src/fd.Base/fd.Base.NHibernate/SchemaRecreator.cs
@@ -1,19 +1,48 @@
+using System.IO;
 using NHibernate.Cfg;
 using NHibernate.Tool.hbm2ddl;
 
 namespace fd.Base.NHibernate
 {
+    /// <summary>
+    /// Drops and re-creates the schema.
+    /// </summary>
     public class SchemaRecreator : IRawNHibernateConfigChanger
     {
-        #region IRawNHibernateConfigChanger Members
+        private readonly bool _outputSql;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SchemaRecreator"/> class.
+        /// </summary>
+        /// <param name="outputSql">
+        /// If set to <c>true</c> outputs the generated SQL to files in the directory <c>NHibernate SQL</c> in the users temp directory.
+        /// </param>
+        public SchemaRecreator(bool outputSql)
+        {
+            _outputSql = outputSql;
+        }
+
+        /// <summary>
+        /// Changes the raw NHibernate config.
+        /// </summary>
+        /// <param name="config">
+        /// The configuration to change.
+        /// </param>
         public void ChangeRawConfig(Configuration config)
         {
             var schemaExport = new SchemaExport(config);
-            schemaExport.Drop(false, true);
-            schemaExport. /*SetOutputFile(@"C:\Temp\Download\MyDDL.sql").*/Create(false, true);
-        }
+            var path = string.Empty;
+            if (_outputSql)
+            {
+                path = Path.Combine(Path.GetTempPath(), "NHibernate SQL");
+                Directory.CreateDirectory(path);
+                schemaExport.SetOutputFile(Path.Combine(path, "drop.sql"));
+            }
 
-        #endregion
+            schemaExport.Drop(false, true);
+            if (_outputSql)
+                schemaExport.SetOutputFile(Path.Combine(path, "create.sql"));
+            schemaExport.Create(false, true);
+        }
     }
 }
