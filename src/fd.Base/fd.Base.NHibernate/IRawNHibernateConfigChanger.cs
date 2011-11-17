5574bf72a006f47cdc6ed81b0e20bf9eeb73567c
Daniel Hilgarth <d.hilgarth@fire-development.com>
2011-11-17 21:05:08 +0100

    Commented interface

--- src/fd.Base/fd.Base.NHibernate/IRawNHibernateConfigChanger.cs
+++ src/fd.Base/fd.Base.NHibernate/IRawNHibernateConfigChanger.cs
@@ -2,8 +2,15 @@ using NHibernate.Cfg;
 
 namespace fd.Base.NHibernate
 {
+    /// <summary>
+    /// Represents a piece of encapsulated logic that changes the raw NHibernate configuration.
+    /// </summary>
     public interface IRawNHibernateConfigChanger
     {
+        /// <summary>
+        /// Changes the raw NHibernate config.
+        /// </summary>
+        /// <param name="config">The configuration to change.</param>
         void ChangeRawConfig(Configuration config);
     }
 }
