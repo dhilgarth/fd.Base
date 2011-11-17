42522ed540f39e6ee3ebe89909bfd0987a2789bd
Daniel Hilgarth <d.hilgarth@fire-development.com>
2011-11-17 21:04:53 +0100

    Added extension method that returns the list an enumerable really is - if it is no list, it creates a new one.

--- src/fd.Base/fd.Base.Extensions.Simple/EnumerableExtensions.cs
+++ src/fd.Base/fd.Base.Extensions.Simple/EnumerableExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace fd.Base.Extensions.Simple
 {
@@ -22,6 +23,25 @@ namespace fd.Base.Extensions.Simple
         public static IEnumerable<T> MakeEnumerable<T>(this T obj)
         {
             yield return obj;
+        }
+
+        /// <summary>
+        /// Converts an <see cref="IEnumerable{T}"/> to an <see cref="IList{T}"/>. If the enumerable already is in fact a <see cref="IList{T}"/>,
+        ///   no new <see cref="IList{T}"/> will be created. Otherwise, <see cref="Enumerable.ToList{TSource}"/> will be used.
+        /// </summary>
+        /// <typeparam name="T">
+        /// The type of the objects in the <see cref="IEnumerable{T}"/>.
+        /// </typeparam>
+        /// <param name="enumerable">
+        /// The enumerable.
+        /// </param>
+        /// <returns>
+        /// An <see cref="IList{T}"/> containing the items of the specified enumerable.
+        /// </returns>
+        public static IList<T> ToListCheap<T>(this IEnumerable<T> enumerable)
+        {
+            var result = enumerable as IList<T>;
+            return result ?? enumerable.ToList();
         }
     }
 }
