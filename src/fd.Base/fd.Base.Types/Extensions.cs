ab0a3c2c0dec105b6324f1ebadbdd80467b277b6
Daniel Hilgarth <d.hilgarth@fire-development.com>
2011-11-27 16:06:40 +0100

    Added Percent extension method.

---
+++ src/fd.Base/fd.Base.Types/Extensions.cs
@@ -0,0 +1,18 @@
+﻿namespace fd.Base.Types
+{
+    /// <summary>
+    /// Provides extension methods for the types defined in this assembly.
+    /// </summary>
+    public static class Extensions
+    {
+        /// <summary>
+        /// Converts the value to a <see cref="Percentage"/> instance.
+        /// </summary>
+        /// <param name="value">The value.</param>
+        /// <returns>The <see cref="Percentage"/> value.</returns>
+        public static Percentage Percent(this int value)
+        {
+            return new Percentage(value / 100.0);
+        }
+    }
+}
