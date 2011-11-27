f86259829107b7f32b15ebd05e9504424749559b
Daniel Hilgarth <d.hilgarth@fire-development.com>
2011-11-27 16:05:54 +0100

    Removed bogus ForeignKey format convention.

--- src/fd.Base/fd.Base.NHibernate/Extensions.cs
+++ src/fd.Base/fd.Base.NHibernate/Extensions.cs
@@ -17,8 +17,9 @@ namespace fd.Base.NHibernate
         /// <returns>Returns the result of the add operation.</returns>
         public static T AddDefault<T>(this SetupConventionFinder<T> conventions)
         {
-            conventions.Add(DefaultCascade.All(), /*Table.Is(x => x.EntityType.Name.Replace("Entity", "")),*/
-                            ForeignKey.Format((me, t) => (me != null ? me.Name : t.Name) + "Id"));
+            conventions.Add(
+                DefaultCascade.All()/*, Table.Is(x => x.EntityType.Name.Replace("Entity", "")),
+                ForeignKey.Format((me, t) => (me != null ? me.Name : t.Name) + "Id")*/);
             return conventions.AddFromAssemblyOf<EnumConvention>();
         }
 
