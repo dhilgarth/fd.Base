2e2d3cc067819311e0acc94bbfa064b74fcbfa04
Daniel Hilgarth <d.hilgarth@fire-development.com>
2012-01-24 21:13:51 +0100

    Updated session factory builder interface to allow reuse of implementations with different assemblies.

--- src/fd.Base/fd.Base.NHibernate/ISessionFactoryBuilder.cs
+++ src/fd.Base/fd.Base.NHibernate/ISessionFactoryBuilder.cs
@@ -1,9 +1,42 @@
+using System.Collections.Generic;
+using System.Reflection;
 using NHibernate;
 
 namespace fd.Base.NHibernate
 {
+    /// <summary>
+    ///   Represents a session factory builder.
+    /// </summary>
     public interface ISessionFactoryBuilder
     {
-        ISessionFactory Build();
+        /// <summary>
+        ///   Builds the session factory using auto mapping only.
+        /// </summary>
+        /// <param name="entityAssemblies"> The assemblies containing the entities to map. </param>
+        /// <param name="conventionAssemblies"> The assemblies containing additional conventions to use. </param>
+        /// <param name="overridesAssemblies"> The assemblies containing mapping overrides. </param>
+        /// <returns> The newly built session factory. </returns>
+        ISessionFactory Build(IEnumerable<Assembly> entityAssemblies, IEnumerable<Assembly> conventionAssemblies, IEnumerable<Assembly> overridesAssemblies);
+
+        /// <summary>
+        ///   Builds the session factory using both fluent and auto mapping.
+        /// </summary>
+        /// <param name="mappingsAssemblies"> The assemblies containing the class maps. </param>
+        /// <param name="entityAssemblies"> The assemblies containing the entities to map. </param>
+        /// <param name="conventionAssemblies"> The assemblies containing additional conventions to use. </param>
+        /// <param name="overridesAssemblies"> The assemblies containing mapping overrides. </param>
+        /// <returns> The newly built session factory. </returns>
+        ISessionFactory Build(
+            IEnumerable<Assembly> mappingsAssemblies, 
+            IEnumerable<Assembly> entityAssemblies, 
+            IEnumerable<Assembly> conventionAssemblies, 
+            IEnumerable<Assembly> overridesAssemblies);
+
+        /// <summary>
+        ///   Builds the session factory using fluent mapping only.
+        /// </summary>
+        /// <param name="mappingsAssemblies"> The assemblies containing the class maps. </param>
+        /// <returns> The newly built session factory. </returns>
+        ISessionFactory Build(IEnumerable<Assembly> mappingsAssemblies);
     }
 }
