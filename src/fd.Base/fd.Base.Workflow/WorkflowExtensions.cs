3996b3028b3e14f28e2f9acc30010db6a5317db4
Daniel Hilgarth <d.hilgarth@fire-development.com>
2012-01-29 17:54:56 +0100

    Added extension method for optional WF arguments

--- src/fd.Base/fd.Base.Workflow/WorkflowExtensions.cs
+++ src/fd.Base/fd.Base.Workflow/WorkflowExtensions.cs
@@ -11,5 +11,21 @@ namespace fd.Base.Workflow
         {
             value.Extensions.Add(new Tracker());
         }
+
+        /// <summary>
+        /// Gets the value of the specified argument or the default value.
+        /// </summary>
+        /// <typeparam name="T">The type of the argument</typeparam>
+        /// <param name="argument">The argument.</param>
+        /// <param name="context">The context.</param>
+        /// <param name="defaultValue">The default value to return of no value was set for the argument.</param>
+        /// <returns>The value of the argument if one was set; otherwise, the specified default value.</returns>
+        public static T Get<T>(this InArgument<T> argument, ActivityContext context, T defaultValue)
+        {
+            var obj = argument.Get(context);
+            if (Equals(obj, default(T)) && argument.Expression == null)
+                obj = defaultValue;
+            return obj;
+        }
     }
 }
